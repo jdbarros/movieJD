0a8ac0e34c63ff521a3ee13f14c88ecf8ab3550e
jdbarros <jdiegozhou@gmail.com>
2022-03-08 16:18:05 -0300

    first commit

---
+++ backend/src/test/java/com/example/ddsmovie/DdsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ddsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
